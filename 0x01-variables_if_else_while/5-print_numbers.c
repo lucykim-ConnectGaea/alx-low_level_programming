4fd98a9a36efe8b705ab1850039113629685ce49
Lucy Kim <njambily@gmail.com>
2023-03-17 12:49:04 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: Print numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("\n");
+	return (0);
+}
