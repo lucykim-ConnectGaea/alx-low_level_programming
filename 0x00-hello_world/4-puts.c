110efddfaac6e1cc29c222fc55934cd3b1c8ffe1
Lucy Kim <njambily@gmail.com>
2023-03-17 06:17:18 -0700

    puts function fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  *main -entry point
  *return: always0(successful)
  */
-int main(0)
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
