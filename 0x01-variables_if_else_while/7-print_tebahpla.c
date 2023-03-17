107d10927907d596a88654fb1e2f45e118f7d1dd
Lucy Kim <njambily@gmail.com>
2023-03-17 13:36:08 -0700

    reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: Print numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i <= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
