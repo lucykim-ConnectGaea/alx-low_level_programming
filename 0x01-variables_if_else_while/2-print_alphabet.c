81db03150d83c93182214ef258a4abfd5285951a
Lucy Kim <njambily@gmail.com>
2023-03-17 11:46:55 -0700

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: Null value
+ *
+ * Description: Alphabets
+ * Return:0
+ */
+
+int main(void)
+{
+	char f;
+
+	for (f = 'a'; c <= 'z'; f++)
+		putchar(f);
+	putchar('\n');
+	return (0);
+}
