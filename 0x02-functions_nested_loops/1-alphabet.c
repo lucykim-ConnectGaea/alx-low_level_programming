84f0a19759051a3ccffc9babc91503ba71a70b61
Lucy Kim <njambily@gmail.com>
2023-03-21 14:36:48 -0700

    aphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void);
+{
+	char j;
+	for (j = 'a';j <= 'z'; j++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+/**
+ * main -check the code
+ * @void: null value
+ * Description: alphabets
+ * Return : 0.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
