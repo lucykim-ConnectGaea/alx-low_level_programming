7094da14bfc229119532cfb7572fe404a7a0612e
Lucy Kim <njambily@gmail.com>
2023-03-23 13:08:02 -0700

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints tens times the numbers
+ * @void: Null value
+ *
+ * Description: prints 10 times the numbers
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	for (i = 0; i < 14; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		{
+			_putchar (i);
+		}
+		{
+			_putchar ('\n');
+		}
+}
