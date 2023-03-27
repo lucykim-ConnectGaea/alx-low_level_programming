78eda8287ca5091540aa020d975a4770554f2163
Lucy Kim <njambily@gmail.com>
2023-03-27 13:04:50 -0700

    returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: arg s
+ *
+ * Return: String length
+ */
+
+int _strlen(char *s)
+{
+	return strlen(s);
+}
