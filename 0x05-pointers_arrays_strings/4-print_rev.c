c72aa661687629017c8443b06acd7b5b6a6670b4
Lucy Kim <njambily@gmail.com>
2023-03-27 13:57:03 -0700

    function that prints a string, in reverse, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _puts - function that prints a string
+  *
+  * @str: parameter
+  * Return: void
+  */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i--;
+	}
+	_putchar('\n');
+}
