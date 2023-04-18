578eb6cb751839454c6d0c10be2b800f2b530364
Lucy Kim <connectgaea@gmail.com>
2023-04-18 04:37:33 -0700

    Define a new type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,9 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_pup(char *name, float age, char *owner);
 
 #endif
