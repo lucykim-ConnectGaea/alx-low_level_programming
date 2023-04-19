cf2542a82d7f20abd0d95bbf7cb9baee3d3d18ed
Lucy Kim <connectgaea@gmail.com>
2023-04-19 04:07:53 -0700

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,20 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - function
+ * @s: operator passed by user
+ * Return: Int
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function for addition
+ * @a: int 1
+ * @b: int 2
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtraction fun
+ * @a: int1
+ * @b: int 2
+ * Return: subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function for multiplication
+ * @a: int 1
+ * @b: int 2
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function for division
+ * @a: int 1
+ * @b: int 2
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function for modulus
+ * @a: int 1
+ * @b: inte 2
+ * Return: modulus
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
