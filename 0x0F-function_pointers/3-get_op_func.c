354885158fbbb8deda54a62cdf0b2f8c8826debb
Lucy Kim <connectgaea@gmail.com>
2023-04-19 05:06:13 -0700

    program that performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,7 @@
 #include "3-calc.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * get_op_func - function
@@ -16,5 +19,12 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,46 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - fucntion
+ * @argc: argument count
+ * @argv: value
+ * Return: error or result
+ */
+
+int main(int argc, char **argv)
+{
+	int num1;
+	int num2;
+	char get_op;
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		{
+			return (98);
+		}
+	}
+
+	num1 = atoi(argv[1]);
+	get_op = argv[2][0];
+	num2 = atoi(argv[3]);
+	result = (get_op == '+') ? num1 + num2 :
+		(get_op == '-') ? num1 - num2 :
+		(get_op == '*') ? num1 * num2 :
+		(get_op == '/' && num2 != 0) ? num1 / num2 :
+		(get_op == '%' && num2 != 0) ? num1 % num2 :
+		printf("Error\n") || 99;
+	if (!(get_op == '+' ||
+				get_op == '-' ||
+				get_op == '*' ||
+				(get_op == '/' && num2 != 0) ||
+				(get_op == '%' && num2 != 0)))
+	{
+		printf("Error\n");
+		return (99);
+	}
+	return (result);
+}
