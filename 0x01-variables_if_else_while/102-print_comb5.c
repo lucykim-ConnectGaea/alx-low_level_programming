24022f718786d1d4484d92852fb7f70eb70eb67d
Lucy Kim <connectgaea@gmail.com>
2023-05-01 03:59:09 -0700

    possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - function entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int j;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (j = a; j <= 99; j++)
+		{
+			if (a == j)
+			{
+				continue;
+			}
+			if (a < 10)/**ddts with 0 at the bgn*/
+			{
+				putchar('0');
+			}
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
+
+			putchar (' ');
+
+			if (j < 10)
+			{
+				putchar('0');
+			}
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (a != 99 || j != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
