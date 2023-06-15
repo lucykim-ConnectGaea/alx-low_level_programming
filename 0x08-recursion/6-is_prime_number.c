34aa84cbe09546c91539e91abf59b01190af5d5b
lucykim-ConnectGaea <connectgaea@gmail.com>
2023-06-15 09:29:17 -0700

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,6 +15,15 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
+	if (n ==2 || n == 3)
+	{
+		return (1);
+	}
+	if (n %2 == 0 || n % 3 == 0)
+	{
+		return (0);
+	}
+
 	for (i = 2; i * i <= n; i++)
 	{
 		if (n % i == 0)
