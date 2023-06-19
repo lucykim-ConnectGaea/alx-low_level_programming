938b32197c359fd5bacee6118aba7dc118859c3d
lucykim-ConnectGaea <connectgaea@gmail.com>
2023-06-19 12:26:17 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - structure
+ * @name:element 1
+ * @age: element 2
+ * @owner:element 3
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_pup(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
+
