30045a487b8dbf4f9ec5e7d5961bc67a361a56e2
lucykim-ConnectGaea <connectgaea@gmail.com>
2023-06-19 12:03:23 -0700

    program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function that name of file
+ * it was comiled from
+ *
+ * Description: Uses th _FILE_
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
